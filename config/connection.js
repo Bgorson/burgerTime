364d04c1d4ecd728120d096e5f9e00d5fc16e387
BGorson <BGorson32@gmail.com>
2019-03-14 19:04:07 -0500

    added mysql

--- config/connection.js
+++ config/connection.js
@@ -3,17 +3,17 @@ const keys = require("../login.js")
 const password = keys.mySQL.pass
 // Creates connection to MYSQL Database uses .env for password
 var connection = mysql.createConnection({
-    host: "localhost",
+    host: "us-cdbr-iron-east-03.cleardb.net",
 
     // Your port; if not 3306
     port: 3306,
 
     // Your username
-    user: "root",
+    user: "bb341b64706ddc",
 
     // Your password
-    password: password,
-    database: "burgers_db"
+    password: 'd31463ce',
+    database: "heroku_22ad994ed484a71"
 });
 
 module.exports = connection;
